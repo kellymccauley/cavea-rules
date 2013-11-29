1ad7f23f05946dfd1e410c2cf518f0a29f11f972
Kelly McCauley <kelly.gitdev@99kayaks.com>
2013-11-29 16:13:13 -0600

    Bumped the version numbers.

--- taskSets.js
+++ taskSets.js
@@ -39,7 +39,7 @@ context.loadWith({
   },
 
   'rots': {
-    version: '0.2.0',
+    version: '0.2.2',
     inceptionYear: '2013',
     author: 'Kelly A. McCauley',
     srcFile: './src/web/rules/index.html',
